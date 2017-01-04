e2511922dea0cc0f7f76d41b8c940a165e866d9a
lshapz <laurashapiro01@gmail.com>
2017-01-04 16:32:36 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+require 'prime'
+require 'byebug'
+def prime_number_for(x)
+
+primes = []
+num = 2 
+
+while primes.length < x
+  
+  if num.prime? 
+    primes << num
+  end 
+  num += 1
+  
+end 
+
+primes.last
+
+end 
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class Prime
+  attr_accessor :number
+
+  def initialize(x)
+    @number = prime_number_for(x)
+
+  end
+
+  def prime_number_for(x)
+
+  primes = []
+  num = 2 
+
+  while primes.length < x
+    
+    if num.prime? 
+      primes << num
+    end 
+    num += 1
+    
+  end 
+
+  primes.last
+
+  end 
+
+end 
